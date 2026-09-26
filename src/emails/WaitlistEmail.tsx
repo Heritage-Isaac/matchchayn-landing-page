2b802fbd0c46e2b8a7719f19d0ec5c2380937db7
Heritage Isaac <dipedeji@gmail.com>
2026-09-26 14:09:54 +0100

    chore: migrate email images to cloudinary and add upload script

--- src/emails/WaitlistEmail.tsx
+++ src/emails/WaitlistEmail.tsx
@@ -21,8 +21,7 @@ interface WaitlistEmailProps {
   firstName: string;
 }
 
-// Reverting to root domain. The image mismatch warning is negligible.
-const baseUrl = process.env.NODE_ENV === "development" ? "" : "https://matchchayn.com";
+const baseUrl = "https://res.cloudinary.com/dwuaixu4c/image/upload/matchchayn-emails";
 
 export default function WaitlistEmail({ firstName = "Alex" }: WaitlistEmailProps) {
   return (
@@ -77,7 +76,7 @@ export default function WaitlistEmail({ firstName = "Alex" }: WaitlistEmailProps
             {/* Header */}
             <Section className="px-6 sm:px-10 pt-6 pb-2 text-center bg-[#18181b]">
               <Img
-                src={`${baseUrl}/static/logo-purple.png`}
+                src={`${baseUrl}/logo-purple.png`}
                 width="180"
                 alt="MatchChayn Logo"
                 className="mx-auto block"
@@ -87,7 +86,7 @@ export default function WaitlistEmail({ firstName = "Alex" }: WaitlistEmailProps
             {/* Hero Image */}
             <Section className="w-full bg-[#09090b] pt-2 pb-0 flex justify-center text-center border-b border-solid border-[#27272a]">
               <Img
-                src={`${baseUrl}/static/couple-intro.png`}
+                src={`${baseUrl}/couple-intro.png`}
                 width="280"
                 alt="Happy Couple"
                 className="mx-auto block rounded-t-lg object-contain"
@@ -127,37 +126,37 @@ export default function WaitlistEmail({ firstName = "Alex" }: WaitlistEmailProps
                 <Row>
                   <Column align="center" className="px-2">
                     <Link href="https://x.com/MatchChayn">
-                      <Img src={`${baseUrl}/static/twitter.png`} width="24" height="24" alt="X (Twitter)" />
+                      <Img src={`${baseUrl}/twitter.png`} width="24" height="24" alt="X (Twitter)" />
                     </Link>
                   </Column>
                   <Column align="center" className="px-2">
                     <Link href="https://www.instagram.com/matchchayn/">
-                      <Img src={`${baseUrl}/static/instagram.png`} width="24" height="24" alt="Instagram" />
+                      <Img src={`${baseUrl}/instagram.png`} width="24" height="24" alt="Instagram" />
                     </Link>
                   </Column>
                   <Column align="center" className="px-2">
                     <Link href="https://www.linkedin.com/company/matchchayn">
-                      <Img src={`${baseUrl}/static/linkedin.png`} width="24" height="24" alt="LinkedIn" />
+                      <Img src={`${baseUrl}/linkedin.png`} width="24" height="24" alt="LinkedIn" />
                     </Link>
                   </Column>
                   <Column align="center" className="px-2">
                     <Link href="https://t.me/matchchayn">
-                      <Img src={`${baseUrl}/static/telegram.png`} width="24" height="24" alt="Telegram" />
+                      <Img src={`${baseUrl}/telegram.png`} width="24" height="24" alt="Telegram" />
                     </Link>
                   </Column>
                   <Column align="center" className="px-2">
                     <Link href="https://tiktok.com/@matchchayn">
-                      <Img src={`${baseUrl}/static/tiktok.png`} width="24" height="24" alt="TikTok" />
+                      <Img src={`${baseUrl}/tiktok.png`} width="24" height="24" alt="TikTok" />
                     </Link>
                   </Column>
                   <Column align="center" className="px-2">
                     <Link href="https://www.youtube.com/@MatchChayn">
-                      <Img src={`${baseUrl}/static/youtube.png`} width="24" height="24" alt="YouTube" />
+                      <Img src={`${baseUrl}/youtube.png`} width="24" height="24" alt="YouTube" />
                     </Link>
                   </Column>
                   <Column align="center" className="px-2">
                     <Link href="https://www.facebook.com/share/1Ae6wtP7em">
-                      <Img src={`${baseUrl}/static/facebook.png`} width="24" height="24" alt="Facebook" />
+                      <Img src={`${baseUrl}/facebook.png`} width="24" height="24" alt="Facebook" />
                     </Link>
                   </Column>
                 </Row>
